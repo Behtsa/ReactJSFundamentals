dcaac9d894fd2436d9411af1e4d52281164dc1bd
behtsa <monze_gdbm@hotmail.com>
2016-10-31 20:07:29 -0600

    Tutorial before router

---
+++ app/index.js
@@ -0,0 +1,70 @@
+var USER_DATA = {
+	name: 'Behtsa Gdbm',
+	username: 'Behtsa',
+	image: 'https://avatars3.githubusercontent.com/u/9289461?v=3&s=466'
+}
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ProfilePic = React.createClass({
+	render: function () {
+		return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+	
+	}
+});
+
+var Link = React.createClass({
+	changeUrl: function () {
+		window.location.replace(this.props.href)
+
+	},
+	render: function () {
+		return (
+			<span 
+			style={{color: 'pink', cursor: 'pointer'}} 
+			onClick={this.changeUrl}>
+				{this.props.children}
+			</span>
+		)
+	}
+});
+
+var ProfileLink = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<Link href={'https://github.com/' + this.props.username}>
+				{this.props.username}
+				</Link>
+			</div>
+		)
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function () {
+		return (
+			<div>
+				{this.props.name}
+			</div>
+		)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
+		)
+	}
+});
+
+ReactDOM.render(
+  <Avatar user={USER_DATA} />,
+  document.getElementById('app')
+);

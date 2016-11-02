b2dcfee3b6d58c55f239367aee73cdb69caecae3
behtsa <monze_gdbm@hotmail.com>
2016-11-01 19:33:58 -0600

    Library Raven added

--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
+var Raven = require ('raven-js')
+
+var sentryKey = 'b2bed7b25edd4a95b4563f34fc61a153'
+var sentryApp = '111248'
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
+
+Raven.config(sentryURL).install()
+
 
 ReactDOM.render(
-  routes,
-  document.getElementById('app')
+  routes, document.getElementById('app')
 );
